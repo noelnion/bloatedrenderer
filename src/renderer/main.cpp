3b2190f9b4617523a745b66f302a6ff5a9a69abc
LZ <noelnion@gmail.com>
2025-12-03 00:14:28 +0100

    Added Basic Line Drawing

--- src/renderer/main.cpp
+++ src/renderer/main.cpp
@@ -1,4 +1,60 @@
 #include "tgaimage.hpp"
+#include <algorithm>
+#include <cmath>
+
+void draw_line1(const int sx,const int sy,const int dx, const int dy, TGAImage &img, const TGAColor &color, const float step = 0.001F)
+{
+  const auto nsteps = static_cast<int>(1.0F / step);
+  for (int i = 0; i <= nsteps; i++) {
+    const float t_param = static_cast<float>(i) * step;
+    const int x_to_draw = sx + static_cast<int>(t_param * static_cast<float>(dx - sx));
+    const int y_to_draw = sy + static_cast<int>(t_param * static_cast<float>(dy - sy));
+    img.set(x_to_draw, y_to_draw, color);
+  }
+}
+
+void draw_line2(const int sx,const int sy,const int dx, const int dy, TGAImage &img, const TGAColor &color)
+{
+
+  const int x_min = std::min(sx, dx);
+  const int x_max = std::max(sx, dx);
+  for (int x_t = x_min; x_t <= x_max; x_t++) {
+    const auto t_param = static_cast<float>(x_t-sx)/static_cast<float>(dx-sx);
+    const int y_to_draw = sy + static_cast<int>(t_param * static_cast<float>(dy - sy));
+    img.set(x_t, y_to_draw, color);
+  }
+}
+
+void draw_line3(int sx, int sy, int dx,  int dy, TGAImage &img, const TGAColor &color)
+{
+
+  bool switched = false;
+  const auto slope = static_cast<float>(dy - sy) / static_cast<float>(dx - sx);
+  
+  if(std::abs(slope) > 1){
+	std::swap(sx, sy);
+	std::swap(dx, dy);
+	switched = true;
+  }
+
+    if (sx > dx) {
+    std::swap(sx, dx);
+    std::swap(sy, dy);
+  }
+  
+  const int x_min = std::min(sx, dx);
+  const int x_max = std::max(sx, dx);
+
+  for (int x_t = x_min; x_t <= x_max; x_t++) {
+    const auto t_param = static_cast<float>(x_t - sx) / static_cast<float>(dx - sx);
+    const int y_to_draw = sy + static_cast<int>(t_param * static_cast<float>(dy - sy));
+    if (switched) {
+      img.set(y_to_draw, x_t, color);
+    } else {
+      img.set(x_t, y_to_draw, color);
+    }
+  }
+}
 
 int main([[maybe_unused]]int argc,[[maybe_unused]] const char** argv){
 
@@ -23,10 +79,18 @@ int main([[maybe_unused]]int argc,[[maybe_unused]] const char** argv){
     constexpr int cx = 62;
 	constexpr int cy = 53;
 
-    framebuffer.set(ax, ay, white);
+	//lines
+	draw_line3(ax, ay, bx, by, framebuffer, blue);
+	draw_line3(bx, by, ax, ay, framebuffer, yellow);
+	draw_line3(cx, cy, bx, by, framebuffer, green);
+	draw_line3(ax, ay, cx, cy, framebuffer, red);
+	draw_line3(cx, cy, ax, ay, framebuffer, yellow);
+	draw_line3(ax, ay, bx, by, framebuffer, green);
+
+	framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
     framebuffer.set(cx, cy, white);
-
+	
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
 }
